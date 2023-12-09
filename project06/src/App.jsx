35b9cc4e180b3bbd5bac9cb455a1c4539cf86c8a
satyamev <satyamev701600@gmail.com>
2023-12-09 18:12:53 +0530

    new project

--- project06/src/App.jsx
+++ project06/src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 
 import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/login'
+import Profile from './components/Profile'
 
 function App() {
   
 
   return (
-    <>
+    <UserContextProvider>
        <h1>Hello World</h1>
-    </>
+       <Login />
+       <Profile />
+    </UserContextProvider>
   )
 }
 
